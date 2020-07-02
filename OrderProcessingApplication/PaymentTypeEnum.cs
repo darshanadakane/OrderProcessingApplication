bc4d83289d932b00898cb09552afadaa17be634d
Darshan Adakane <56758509+darshanadakane@users.noreply.github.com>
2020-07-03 01:06:35 +0530

    added payment type enum

---
+++ OrderProcessingApplication/PaymentTypeEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderProcessingApp
+{
+    public enum PaymentType
+    {
+        Book,
+        LearningToSkiVideo,
+        Membership,
+        PhysicalProduct,
+        Upgrade,
+        Video
+    }
+}
